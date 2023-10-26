10687bc545af708c12c916da0307fc48cab2b4bc
DiMatveev1991 <98545906+DiMatveev1991@users.noreply.github.com>
2023-10-26 10:04:17 +0500

    comit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Converter;
+using System.Linq;
+using System;
+
+namespace MyYoutubeClient
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //ссылка на видео
+             Console.WriteLine ("Введите ссылку на ютуб видео для скачивания:");
+            string url = Console.ReadLine();
+            string outPathFile = @"C:\YoutubeClient\";
+            //создание объектов: отправитель, получатель, команда - получение информации о видео
+            var sender = new SenderCommand();
+            var receiver = new Receiver();
+            var getInfoVideo = new GetInfoVideoCommand(receiver);
+
+            /*1. отправка команды
+             *2. получение информации о видео
+            */
+            sender.SetCommand(getInfoVideo);
+            await sender.GetInfo(url);
+
+            /*1. создание команды на скачивание видео
+             *2. отправка команды на скачивание
+             *3. Запуск скачивания
+            */
+            var downloadVideo = new DownloadCommand(receiver);
+            sender.SetCommand(downloadVideo);
+            await sender.DownLoad(url, outPathFile);
+
+           
+        }
+    }
+}
